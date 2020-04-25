66bb38c882b46c1bb6762841cf347f0dbc0ba5ba
Eric <1eric.hoffman@gmail.com>
2020-04-25 15:31:29 -0500

    filled out sauce decorator abstract class

--- src/main/java/SauceDecorator.java
+++ src/main/java/SauceDecorator.java
@@ -1,2 +1,20 @@
-public class SauceDecorator {
+// Each wing order 'has a' sauce
+
+abstract class SauceDecorator implements Wings {
+
+    protected Wings tempWings;
+
+    // Sauces (Decorators) dynamically customize the PlainWings
+    public SauceDecorator(Wings newWings){
+        tempWings = newWings;
+    }
+
+    public String getDescription() {
+        return tempWings.getDescription();
+    }
+
+    public double getCost() {
+        return tempWings.getCost();
+    }
+
 }
