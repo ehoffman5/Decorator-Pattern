e534946b71161403d51b92397ccbe0ece0f136df
Eric <1eric.hoffman@gmail.com>
2020-04-25 15:14:26 -0500

    updated classes and filled out base wing functionality

--- src/main/java/PlainWings.java
+++ src/main/java/PlainWings.java
@@ -1,2 +1,14 @@
-public class PlainWings {
+// Every Pizza made will start as a PlainPizza
+
+public class PlainWings implements Wings {
+
+    public String getDescription() {
+        return "Tasty vegan seitan wings.";
+    }
+
+    public double getCost() {
+        System.out.println("Base Price: $" + 6.00);
+        return 6.00;
+    }
+
 }
--- src/main/java/Wings.java
+++ src/main/java/Wings.java
@@ -1,2 +1,8 @@
-public class Wings {
+// Interface containing basic functions to be used in all other wings
+
+public interface Wings {
+
+    public String getDescription();
+    public double getCost();
+
 }
